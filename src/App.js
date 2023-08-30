ba622496b957cf24fb54f7acb9565795ab2e5164
Arjun Vijayan <vijayanarjun4@gmail.com>
2023-08-30 21:28:30 +0530

    Intro Page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import { Navbar } from './navbar/Navbar';
 import { Loader } from './loader/Loader';
+import { Intro } from './introsection/Intro';
 
 function App() {
   return (
     <div className="app">
       <Loader></Loader>
       <Navbar></Navbar>
+      <Intro></Intro>
     </div>
   )
 }
---
+++ src/introsection/Intro.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './intro.css'
+
+export const Intro = () => {
+  return (
+    <div className='intro'>
+        <div className="intro-content">
+            <h2>Web Developer . Tech Enthusiast . Video Editor</h2>
+            <p>and much more</p>
+        </div>
+    </div>
+  )
+}
