917a84a2f810ad40f2855f1fba1e388c697f8305
Arjun Vijayan <vijayanarjun4@gmail.com>
2023-09-01 00:24:56 +0530

    New mouse Pointer

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import './App.css';
+import { Navbar } from './navbar/Navbar';
+import { Loader } from './loader/Loader';
+import { Intro } from './introsection/Intro';
+import { About } from './About/About';
+import { Contact } from './Contact/Contact';
+import { useEffect, useState } from 'react';
+import { motion } from 'framer-motion';
+
+function App() {
+  //Mouse Cursor Setup
+  const [mousePosition, setMousePosition] = useState({
+    x:0,
+    y:0
+  })
+  console.log(mousePosition)
+
+  useEffect(()=>{
+    const mouseMove=e=>{
+      setMousePosition({
+        x:e.clientX,
+        y:e.clientY
+      })
+    }
+
+    window.addEventListener("mousemove", mouseMove)
+
+    return()=>{
+      window.removeEventListener("mousemove", mouseMove)
+    }
+  },[])
+
+  const variants ={
+    default:{
+      x: mousePosition.x -12.5,
+      y: mousePosition.y -12.5,
+    }
+  }
+  return (
+    <div className="app">
+      <motion.div
+        variants={variants} 
+        animate="default"
+        className="cursor">  
+      </motion.div>
+      <Loader></Loader>
+      <Navbar></Navbar>
+      <Intro></Intro>
+      <About></About>
+      <Contact></Contact>
+    </div>
+  )
+}
+
+export default App;
