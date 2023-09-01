4c008b68d7d624d3216f3950aa2230667f46ad8d
Arjun Vijayan <vijayanarjun4@gmail.com>
2023-09-01 14:20:31 +0530

    Quotes module

--- src/Quotes/Quotes.jsx
+++ src/Quotes/Quotes.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import data from './data/quotes.json'
 import './quotes.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDice } from '@fortawesome/free-solid-svg-icons'
 
 export const Quotes = () => {
-  const quotes = data.Quotes  
+  const quotes = data.Quotes
+  const len = data.Quotes.length
+  const [quote, setQuote] = useState([[quotes[Math.floor(Math.random()*len)]]])  
+
+
+  useEffect(()=>{
+    async function setter(){
+        setQuote([quotes[Math.floor(Math.random()*len)]])
+        console.log(quote)
+        }
+    setter()
+    },[])
+    function setter(){
+        setQuote([quotes[Math.floor(Math.random()*len)]])
+        console.log(quote)
+    }
   return (
     <div className="quotes">
-        <div className="quotetext">
-        <p className='quote'>"{quotes[0].Quote}"</p>
-        <p className='quotename'>-{quotes[0].Name}</p>
+        <div className="dummy">
+            <div className="quotetext">
+                <p className='quote'>"{quote[0].Quote}"</p>
+                <p className='quotename'>-{quote[0].Name}</p>
+            </div>
+        <a type='button' className='resume-button' onClick={setter}><FontAwesomeIcon icon={faDice} /> Change Quote</a>
         </div>
     </div>
   )
